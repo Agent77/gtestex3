f15a4b8714038485fb095cbf8b7031de005043c2
Tiki <lobel.tiki@gmail.com>
2016-12-15 00:01:54 +0200

    Tests fixed

--- TestTaxiCenter.h
+++ TestTaxiCenter.h
@@ -13,11 +13,11 @@ private:
 public:
     void SetUp() {
         tc = TaxiCenter();
-        tc.getLocations()[0] = Point(0,0);
-        tc.getLocations()[1] = Point(1,1);
-        tc.getLocations()[2] = Point(2,2);
-        tc.getTrips()[0] = Trip(Point(3,3), Point(1,1));
-        tc.getTrips()[1] = Trip(Point(2,2), Point(3,4));
+            cout << "Setting up.";
+    }
+    void TearDown() {
+            cout << "Tearing Down.";
+
     }
 };
 
